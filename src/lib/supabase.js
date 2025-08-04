170a83f65b7d5678830378e9363d72bc50d13b7b
Ivan Farfan <ivanfarfan567@yahoo.com>
2025-08-03 21:13:44 -0500

    feat: Added ENVs

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://bqpxuncmbrfdwdgagfls.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxcHh1bmNtYnJmZHdkZ2FnZmxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyNTU3NDEsImV4cCI6MjA2OTgzMTc0MX0.j0dx9JZ27uToa75ROVfwCbEPiJpAnv7TJJNEi8KyL7c';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
